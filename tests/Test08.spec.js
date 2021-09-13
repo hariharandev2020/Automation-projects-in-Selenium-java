5a49cb80899b92144b4a7a746c6899b9a2d48ef8
HariHaran T <72505231+hariharandev2020@users.noreply.github.com>
2021-09-13 23:14:11 +0530

    Date picker test cases updated

--- tests/Test08.spec.js
+++ tests/Test08.spec.js
@@ -1,4 +1,4 @@
-import { Selector } from 'testcafe';
+import { Selector,ClientFunction  } from 'testcafe';
 import page from './method.po';
 
 fixture `Test JQuery UI page`
@@ -10,38 +10,47 @@ fixture `Test JQuery UI page`
     const dialogbox  = Selector('a').withExactText('Dialog');
     await t.click(dialogbox);
 })
-const dialog = Selector('div[role=dialog]');
-const header = Selector('span#ui-id-1');
-const text   = Selector('#dialog p');
-const btn    = Selector('button');
-
 test("Dialogue box Enable check", async t => {
     page.browserscroll();
-    await t.switchToIframe('.demo-frame')
-    .expect(dialog.hasAttribute('disabled')).notOk();
+    await t.switchToIframe('.demo-frame');
+    const dialog = Selector('div[role=dialog]');
+    await t.expect(dialog.hasAttribute('disabled')).notOk();
 })
 test("Dialogue box Text check", async t => {
-     page.browserscroll();
-     await t.switchToIframe('.demo-frame')
+    page.browserscroll();
+    await t.switchToIframe('.demo-frame');
+    const header = Selector('span#ui-id-1');
+    const text   = Selector('#dialog p');
     await t.expect(await header.innerText).eql('Basic dialog')
     await t.expect(await text.innerText).eql("This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.")
 })
 test('Drag the element', async t => {
     page.browserscroll();
-    await t.switchToIframe('.demo-frame')
-    .drag(dialog, 360, 0, {
-        offsetX: 10,
-        offsetY: 10,
-        modifiers: {
-            shift: true
-        }
-    });
+    await t.switchToIframe('.demo-frame');
+    const dialog = Selector('div[role=dialog]');
+    await t.drag(dialog, 360, 0);
     const lf =await dialog.getStyleProperty('left');
-    await t.expect(lf).eql("279px");
+    await t.expect(lf).eql("139.5px");
+})
+test.skip('Resize the dialog box', async t =>{
+    page.browserscroll();
+    await t.switchToIframe('.demo-frame')
+    const changeHammerheadDiv = ClientFunction(() => {
+    const hammerHeadDiv = document.querySelector('body div:nth-child(1)');
+    hammerHeadDiv.style.width =  '900';
+});
+// await t.switchToIframe('.demo-frame');
+await changeHammerheadDiv();
+wait(2000)
+const width = dialog.getStyleProperty('width');
+console.log(await dialog.width);
 })
 test('Close the dialog', async t =>{
     page.browserscroll();
-    await t.switchToIframe('.demo-frame')
-    .click(btn)
+    await t.switchToIframe('.demo-frame');
+    const dialog = Selector('div[role=dialog]');
+    const btn    = Selector('button');
+    await t.click(btn)
     .expect(dialog.visible).notOk();
 })
+
