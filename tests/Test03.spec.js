14af6c4c51b52262ebcc43d46c9bdea13bb36086
HariHaran T <72505231+hariharandev2020@users.noreply.github.com>
2021-09-13 17:39:09 +0530

    Change the global varaiables into local variables

--- tests/Test03.spec.js
+++ tests/Test03.spec.js
@@ -10,76 +10,83 @@ fixture `Test JQuery UI page`
     const radioBtn  = Selector('a').withExactText('Checkboxradio');
     await t.click(radioBtn);
 })
-    const iframe    = Selector(".demo-frame"); 
-    const r1 = Selector('input[type=radio]').nth(0);
-    const r2 = Selector('input[type=radio]').nth(1);
-    const r3 = Selector('input[type=radio]').nth(2);
-    const ex1 = "rgb(0, 127, 255";
-    const ex2 = "rgb(246, 246, 246)";
-    const lbl    = Selector('label[for=radio-1]');
-    const lbl1    = Selector('label[for=radio-2]');
-    const lb2    = Selector('label[for=radio-3]');
-
 test('Radio 1', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
+        await t .switchToIframe('.demo-frame')
         const actual = Selector('label[for=radio-1]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' New York', 'check element text', { timeout: 500 });
 })
 test('Radio 2', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
+        await t .switchToIframe('.demo-frame')
         const actual = Selector('label[for=radio-2]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' Paris', 'check element text', { timeout: 500 });   
 })
 test('Radio 3', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
+        await t .switchToIframe('.demo-frame')
         const actual = Selector('label[for=radio-3]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' London', 'check element text', { timeout: 500 });
 
 })  
 test('Radio -4 test', async t => {
+    
     page.browserscroll(); 
-        await t .switchToIframe(iframe)
-        await t.expect(r1.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
-        await t.expect(r2.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
-        await t.expect(r3.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
-        await t
-        .click(Selector('label').withAttribute('for', 'radio-1'))
-        .expect(ex1).ok(r1.getStyleProperty('backgroundcolor'))
-        .expect(lbl.classNames).contains('ui-checkboxradio-checked')
-        .expect(ex2).ok(r2.getStyleProperty('backgroundcolor'))
-        .expect(ex2).ok(r3.getStyleProperty('backgroundcolor'))
-        
+        await t .switchToIframe('.demo-frame')
+        const radio1 = Selector('input[type=radio]').nth(0);
+        const radio2 = Selector('input[type=radio]').nth(1);
+        const radio3 = Selector('input[type=radio]').nth(2);
+        const expect1 = "rgb(0, 127, 255";
+        const expect2 = "rgb(246, 246, 246)";
+        const label    = Selector('label[for=radio-1]');
+            await t.expect(radio1.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
+            await t.expect(radio2.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
+            await t.expect(radio3.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
+            await t
+            .click(Selector('label').withAttribute('for', 'radio-1'))
+            .expect(expect1).ok(radio1.getStyleProperty('backgroundcolor'))
+            .expect(expect2).ok(radio2.getStyleProperty('backgroundcolor'))
+            .expect(expect2).ok(radio3.getStyleProperty('backgroundcolor'))     
 })
 test('Radio -5 test', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
-        await t.expect(r1.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 })
-        await t.expect(r2.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 })
-        await t.expect(r3.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 })
+    const radio1 = Selector('input[type=radio]').nth(0);
+    const radio2 = Selector('input[type=radio]').nth(1);
+    const radio3 = Selector('input[type=radio]').nth(2);
+    const expect1 = "rgb(0, 127, 255";
+    const expect2 = "rgb(246, 246, 246)";
+    const label    = Selector('label[for=radio-1]');
+        await t .switchToIframe('.demo-frame')
+        await t.expect(radio1.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 })
+        await t.expect(radio2.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 })
+        await t.expect(radio3.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 })
         .click(Selector('label').withAttribute('for', 'radio-2'))
-        .expect(ex2).ok(r1.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(r2.getStyleProperty('backgroundcolor'))
-        .expect(lbl.classNames).contains('ui-checkboxradio-checked')
-        .expect(ex2).ok(r3.getStyleProperty('backgroundcolor'));          
+        .expect(expect2).ok(radio1.getStyleProperty('backgroundcolor'))
+        .expect(expect1).ok(radio2.getStyleProperty('backgroundcolor'))
+        .expect(expect2).ok(radio3.getStyleProperty('backgroundcolor'));          
 })
 test('Radio -6 test', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
-        await t.expect(r1.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
-        await t.expect(r2.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
-        await t.expect(r3.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
+    const radio1 = Selector('input[type=radio]').nth(0);
+    const radio2 = Selector('input[type=radio]').nth(1);
+    const radio3 = Selector('input[type=radio]').nth(2);
+    const expect1 = "rgb(0, 127, 255";
+    const expect2 = "rgb(246, 246, 246)";
+    const label    = Selector('label[for=radio-1]');
+    const label1    = Selector('label[for=radio-2]');
+    const labe2    = Selector('label[for=radio-3]');
+        await t .switchToIframe('.demo-frame')
+        await t.expect(radio1.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
+        await t.expect(radio2.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
+        await t.expect(radio3.hasAttribute('disabled')).notOk('ready for testing', { timeout: 5000 });
         await t
         .click(Selector('label').withAttribute('for', 'radio-3'));
         await t
-        .expect(ex2).ok(r1.getStyleProperty('backgroundcolor'))
-        .expect(ex2).ok(r2.getStyleProperty('backgroundcolor'))
-        .expect(lbl.classNames).contains('ui-checkboxradio-checked')
-        .expect(ex1).ok(r3.getStyleProperty('backgroundcolor'));          
+        .expect(expect2).ok(radio1.getStyleProperty('backgroundcolor'))
+        .expect(expect2).ok(radio2.getStyleProperty('backgroundcolor'))
+        .expect(expect1).ok(radio3.getStyleProperty('backgroundcolor'));          
 })
 
