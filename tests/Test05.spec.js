b1e5ac754eca71bbafee329a5c19e3f97bc601db
HariHaran T <72505231+hariharandev2020@users.noreply.github.com>
2021-09-13 18:21:40 +0530

    change the variables into local

--- tests/Test05.spec.js
+++ tests/Test05.spec.js
@@ -11,7 +11,6 @@ fixture `Test JQuery UI page`
     const checkBox  = Selector('a').withExactText('Checkboxradio');
     await t.click(checkBox);
 })
-const iframe = Selector(".demo-frame"); 
 const c5     = Selector('input[type=checkbox]').nth(0);
 const c6     = Selector('input[type=checkbox]').nth(1);
 const c7     = Selector('input[type=checkbox]').nth(2);
@@ -25,92 +24,110 @@ const lbl4    = Selector('label[for=checkbox-nested-4]');
 
 test("Checkbox -5-active", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
-    await t.expect(c5.hasAttribute('disabled')).notOk('ready for testing');        
-    await t.expect(c6.hasAttribute('disabled')).notOk('ready for testing');
-    await t.expect(c7.hasAttribute('disabled')).notOk('ready for testing');
-    await t.expect(c8.hasAttribute('disabled')).notOk('ready for testing')
-        await t.expect(c5.hasAttribute('disabled')).notOk('ready for testing');        
+    await t .switchToIframe('.demo-frame');
+    const checkBox5     = Selector('input[type=checkbox]').nth(0);
+    const checkBox6     = Selector('input[type=checkbox]').nth(1);
+    const checkBox7     = Selector('input[type=checkbox]').nth(2);
+    const checkBox8    = Selector('input[type=checkbox]').nth(3);
+    await t.expect(checkBox5.hasAttribute('disabled')).notOk('ready for testing');        
+    await t.expect(checkBox6.hasAttribute('disabled')).notOk('ready for testing');
+    await t.expect(checkBox7.hasAttribute('disabled')).notOk('ready for testing');
+    await t.expect(checkBox8.hasAttribute('disabled')).notOk('ready for testing')
         const actual = Selector('label[for=checkbox-nested-1]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' 2 Double', 'check element text');
 })
 test("Checkbox -6-active", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
+    await t .switchToIframe('.demo-frame')
     const actual = Selector('label[for=checkbox-nested-2]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' 2 Queen', 'check element text');
 })
 test("Checkbox -7-active", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
+    await t .switchToIframe('.demo-frame')
     const actual = Selector('label[for=checkbox-nested-3]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' 1 Queen', 'check element text');
 })
 test("Checkbox -8-active", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
+    await t .switchToIframe('.demo-frame')
     const actual = Selector('label[for=checkbox-nested-4]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' 1 King', 'check element text');
 })
 test('Nested Checkbox -1-test', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-nested-1'))
-        .expect(lbl1.classNames).contains('ui-checkboxradio-checked');
+        await t .switchToIframe('.demo-frame');
+        const label1   = Selector('label[for=checkbox-nested-1]');
+        await t.click(Selector('label').withAttribute('for', 'checkbox-nested-1'))
+        .expect(label1.classNames).contains('ui-checkboxradio-checked');
 })
 test('Nested Checkbox -2-test', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-nested-2'))
-        .expect(lbl2.classNames).contains('ui-checkboxradio-checked');
+        await t .switchToIframe('.demo-frame');
+        const label2   = Selector('label[for=checkbox-nested-2]');
+        await t.click(Selector('label').withAttribute('for', 'checkbox-nested-2'))
+        .expect(label2.classNames).contains('ui-checkboxradio-checked');
 })
 test('Nested Checkbox -3-test', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-nested-3'))
-        .expect(lbl3.classNames).contains('ui-checkboxradio-checked');
+        await t .switchToIframe('.demo-frame');
+        const label3   = Selector('label[for=checkbox-nested-3]');
+        await t.click(Selector('label').withAttribute('for', 'checkbox-nested-3'))
+        .expect(label3.classNames).contains('ui-checkboxradio-checked');
 })
 test('Nested Checkbox -4-test', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-nested-4'))
-        .expect(lbl4.classNames).contains('ui-checkboxradio-checked');
+        await t .switchToIframe('.demo-frame');
+        const label4   = Selector('label[for=checkbox-nested-4]');
+        await t.click(Selector('label').withAttribute('for', 'checkbox-nested-4'))
+        .expect(label4.classNames).contains('ui-checkboxradio-checked');
 })
 test('Checkbox -1,2 test', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-nested-1'))
+        await t .switchToIframe('.demo-frame');
+    const checkBox5     = Selector('input[type=checkbox]').nth(0);
+    const checkBox6     = Selector('input[type=checkbox]').nth(1);
+    const checkBox7     = Selector('input[type=checkbox]').nth(2);
+    const checkBox8    = Selector('input[type=checkbox]').nth(3);
+    await t.click(Selector('label').withAttribute('for', 'checkbox-nested-1'))
         .click(Selector('label').withAttribute('for', 'checkbox-nested-2'))
-        .expect(ex1).ok(c5.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c6.getStyleProperty('backgroundcolor'))
-        .expect(ex2).ok(c7.getStyleProperty('backgroundcolor')) 
-        .expect(ex2).ok(c8.getStyleProperty('backgroundcolor')); 
+        .expect(ex1).ok(checkBox5.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox6.getStyleProperty('backgroundcolor'))
+        .expect(ex2).ok(checkBox7.getStyleProperty('backgroundcolor')) 
+        .expect(ex2).ok(checkBox8.getStyleProperty('backgroundcolor')); 
 })
 test("Checkbox -1,2,3",async t =>{
     page.browserscroll();
-    await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-nested-1'))
+    await t .switchToIframe('.demo-frame');
+    const checkBox5     = Selector('input[type=checkbox]').nth(0);
+    const checkBox6     = Selector('input[type=checkbox]').nth(1);
+    const checkBox7     = Selector('input[type=checkbox]').nth(2);
+    const checkBox8    = Selector('input[type=checkbox]').nth(3);
+    await t.click(Selector('label').withAttribute('for', 'checkbox-nested-1'))
         .click(Selector('label').withAttribute('for', 'checkbox-nested-2'))
         .click(Selector('label').withAttribute('for', 'checkbox-nested-3'))
-        .expect(ex1).ok(c5.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c6.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c7.getStyleProperty('backgroundcolor'))
-        .expect(ex2).ok(c8.getStyleProperty('backgroundcolor')); 
+        .expect(ex1).ok(checkBox5.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox6.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox7.getStyleProperty('backgroundcolor'))
+        .expect(ex2).ok(checkBox8.getStyleProperty('backgroundcolor')); 
 })
 test('Checkbox 1,2,3,4 test', async t =>{
     page.browserscroll();
-    await t .switchToIframe(iframe)
-    await t
-        .click(Selector('label').withAttribute('for', 'checkbox-nested-1'))
+    await t.switchToIframe('.demo-frame');
+    const checkBox5     = Selector('input[type=checkbox]').nth(0);
+    const checkBox6     = Selector('input[type=checkbox]').nth(1);
+    const checkBox7     = Selector('input[type=checkbox]').nth(2);
+    const checkBox8    = Selector('input[type=checkbox]').nth(3);
+    await t.click(Selector('label').withAttribute('for', 'checkbox-nested-1'))
         .click(Selector('label').withAttribute('for', 'checkbox-nested-2'))
         .click(Selector('label').withAttribute('for', 'checkbox-nested-3'))
         .click(Selector('label').withAttribute('for', 'checkbox-nested-4'))
-        .expect(ex1).ok(c5.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c6.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c7.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c8.getStyleProperty('backgroundcolor'));  
+        .expect(ex1).ok(checkBox5.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox6.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox7.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox8.getStyleProperty('backgroundcolor'));  
 })
