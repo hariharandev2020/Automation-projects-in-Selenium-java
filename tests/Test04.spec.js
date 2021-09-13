6107877ae028b079608f06b02629a5464302c8e9
HariHaran T <72505231+hariharandev2020@users.noreply.github.com>
2021-09-13 18:09:30 +0530

    change the variables global into local

--- tests/Test04.spec.js
+++ tests/Test04.spec.js
@@ -10,108 +10,123 @@ fixture `Test JQuery UI page`
     const checkBox  = Selector('a').withExactText('Checkboxradio');
     await t.click(checkBox);
 })
-const iframe = Selector(".demo-frame"); 
-const c1     = Selector('input[type=checkbox]').nth(0);
-const c2     = Selector('input[type=checkbox]').nth(1);
-const c3     = Selector('input[type=checkbox]').nth(2);
-const c4     = Selector('input[type=checkbox]').nth(3);
-const ex1    = "rgb(0, 127, 255";
-const ex2    = "rgb(246, 246, 246)";
-const lbl1   = Selector('label[for=checkbox-1]');
-const lbl2   = Selector('label[for=checkbox-2]');
-const lbl3    = Selector('label[for=checkbox-3]');
-const lbl4    = Selector('label[for=checkbox-4]');
-
 test("Checkbox -1-active", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
-    await t.expect(c1.hasAttribute('disabled')).notOk('ready for testing');        
-    await t.expect(c2.hasAttribute('disabled')).notOk('ready for testing');
-    await t.expect(c3.hasAttribute('disabled')).notOk('ready for testing');
-    await t.expect(c4.hasAttribute('disabled')).notOk('ready for testing')
-        await t.expect(c1.hasAttribute('disabled')).notOk('ready for testing');        
+    await t .switchToIframe('.demo-frame');
+    const checkBox1     = Selector('input[type=checkbox]').nth(0);
+    const checkBox2     = Selector('input[type=checkbox]').nth(1);
+    const checkBox3     = Selector('input[type=checkbox]').nth(2);
+    const checkBox4     = Selector('input[type=checkbox]').nth(3);
+    await t.expect(checkBox1.hasAttribute('disabled')).notOk('ready for testing');        
+    await t.expect(checkBox2.hasAttribute('disabled')).notOk('ready for testing');
+    await t.expect(checkBox3.hasAttribute('disabled')).notOk('ready for testing');
+    await t.expect(checkBox4.hasAttribute('disabled')).notOk('ready for testing')
         const actual = Selector('label[for=checkbox-1]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' 2 Star', 'check element text');
 })
 test("Checkbox -2-active", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
+    await t .switchToIframe('.demo-frame');
         const actual = Selector('label[for=checkbox-2]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' 3 Star', 'check element text');
 })
 test("Checkbox -3-active", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
-        .expect(c3.hasAttribute('disabled')).notOk('ready for testing');
+    await t .switchToIframe(".demo-frame");
+    const checkBox3     = Selector('input[type=checkbox]').nth(2);
+        await t.expect(checkBox3.hasAttribute('disabled')).notOk('ready for testing');
         const actual = Selector('label[for=checkbox-3]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' 4 Star', 'check element text', { timeout: 500 });
 })
 test("Checkbox -4-active", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
-        .expect(c4.hasAttribute('disabled')).notOk('ready for testing')
+    await t .switchToIframe('.demo-frame')
+    const checkBox4     = Selector('input[type=checkbox]').nth(3);
+    await t.expect(checkBox4.hasAttribute('disabled')).notOk('ready for testing')
         const actual = Selector('label[for=checkbox-4]');
         const txt = await actual.innerText;
         await t.expect(txt.toString()).eql(' 5 Star', 'check element text', { timeout: 500 });
 })
 test("Checkbox-1", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-1'));   
-        await t.expect(lbl1.classNames).contains('ui-checkboxradio-checked')
+    await t .switchToIframe(".demo-frame");
+    const label1   = Selector('label[for=checkbox-1]');
+    await t.click(Selector('label').withAttribute('for', 'checkbox-1'));   
+        await t.expect(label1.classNames).contains('ui-checkboxradio-checked')
 })
 test("Checkbox-2", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-2'));   
-        await t.expect(lbl2.classNames).contains('ui-checkboxradio-checked')
+    await t .switchToIframe(".demo-frame");
+    const label2   = Selector('label[for=checkbox-2]');
+    await t.click(Selector('label').withAttribute('for', 'checkbox-2'));   
+        await t.expect(label2.classNames).contains('ui-checkboxradio-checked')
 })
 test("Checkbox-3", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-3'));   
-        await t.expect(lbl3.classNames).contains('ui-checkboxradio-checked')
+    await t .switchToIframe('.demo-frame');
+    const label3   = Selector('label[for=checkbox-3]');
+    await t.click(Selector('label').withAttribute('for', 'checkbox-3'));   
+        await t.expect(label3.classNames).contains('ui-checkboxradio-checked')
 })
 test("Checkbox-4", async t => {
     page.browserscroll();
-    await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-4'));   
-        await t.expect(lbl4.classNames).contains('ui-checkboxradio-checked')
+    await t .switchToIframe('.demo-frame');
+    const label4   = Selector('label[for=checkbox-4]');
+    await t.click(Selector('label').withAttribute('for', 'checkbox-4'));   
+    await t.expect(label4.classNames).contains('ui-checkboxradio-checked')
 })
-test.skip('Checkbox -1,2 test', async t => {
+test('Checkbox -1,2 test', async t => {
     page.browserscroll();
-        await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-1'))
+    await t .switchToIframe('.demo-frame');
+    const checkBox1     = Selector('input[type=checkbox]').nth(0);
+    const checkBox2     = Selector('input[type=checkbox]').nth(1);
+    const checkBox3     = Selector('input[type=checkbox]').nth(2);
+    const checkBox4     = Selector('input[type=checkbox]').nth(3);
+    const ex1    = "rgb(0, 127, 255";
+    const ex2    = "rgb(246, 246, 246)";
+    await t.click(Selector('label').withAttribute('for', 'checkbox-1'))
         .click(Selector('label').withAttribute('for', 'checkbox-2'))
-        .expect(ex1).ok(c1.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c2.getStyleProperty('backgroundcolor'))
-        .expect(ex2).ok(c3.getStyleProperty('backgroundcolor')) 
-        .expect(ex2).ok(c4.getStyleProperty('backgroundcolor')); 
+        .expect(ex1).ok(checkBox1.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox2.getStyleProperty('backgroundcolor'))
+        .expect(ex2).ok(checkBox3.getStyleProperty('backgroundcolor')) 
+        .expect(ex2).ok(checkBox4.getStyleProperty('backgroundcolor')); 
 })
 test("Checkbox -1,2,3",async t =>{
     page.browserscroll();
-    await t .switchToIframe(iframe)
-        .click(Selector('label').withAttribute('for', 'checkbox-1'))
+    await t .switchToIframe('.demo-frame');
+    const checkBox1     = Selector('input[type=checkbox]').nth(0);
+    const checkBox2     = Selector('input[type=checkbox]').nth(1);
+    const checkBox3     = Selector('input[type=checkbox]').nth(2);
+    const checkBox4     = Selector('input[type=checkbox]').nth(3);
+    const ex1    = "rgb(0, 127, 255";
+    const ex2    = "rgb(246, 246, 246)";
+    await t.click(Selector('label').withAttribute('for', 'checkbox-1'))
         .click(Selector('label').withAttribute('for', 'checkbox-2'))
         .click(Selector('label').withAttribute('for', 'checkbox-3'))
-        .expect(ex1).ok(c1.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c2.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c3.getStyleProperty('backgroundcolor'))
-        .expect(ex2).ok(c4.getStyleProperty('backgroundcolor')); 
+        .expect(ex1).ok(checkBox1.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox2.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox3.getStyleProperty('backgroundcolor'))
+        .expect(ex2).ok(checkBox4.getStyleProperty('backgroundcolor')); 
 })
 test('Checkbox 1,2,3,4 test', async t =>{
     page.browserscroll();
-    await t .switchToIframe(iframe)
+    await t .switchToIframe('.demo-frame');
+    const checkBox1     = Selector('input[type=checkbox]').nth(0);
+    const checkBox2     = Selector('input[type=checkbox]').nth(1);
+    const checkBox3     = Selector('input[type=checkbox]').nth(2);
+    const checkBox4     = Selector('input[type=checkbox]').nth(3);
+    const ex1    = "rgb(0, 127, 255";
+    const ex2    = "rgb(246, 246, 246)";
     await t
         .click(Selector('label').withAttribute('for', 'checkbox-1'))
         .click(Selector('label').withAttribute('for', 'checkbox-2'))
         .click(Selector('label').withAttribute('for', 'checkbox-3'))
         .click(Selector('label').withAttribute('for', 'checkbox-4'))
-        .expect(ex1).ok(c1.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c2.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c3.getStyleProperty('backgroundcolor'))
-        .expect(ex1).ok(c4.getStyleProperty('backgroundcolor'));  
+        .expect(ex1).ok(checkBox1.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox2.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox3.getStyleProperty('backgroundcolor'))
+        .expect(ex1).ok(checkBox4.getStyleProperty('backgroundcolor'));  
 })
