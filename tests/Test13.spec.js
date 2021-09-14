616014ff0b8ad3d7a8586c28a0d7586ec68a1039
HariHaran T <72505231+hariharandev2020@users.noreply.github.com>
2021-09-14 11:38:37 +0530

    Add up and down button check for input

--- tests/Test13.spec.js
+++ tests/Test13.spec.js
@@ -3,26 +3,20 @@ import page from './method.po';
 
 fixture `Test JQuery UI page`
 .page `https://jqueryui.com/`
-
 .beforeEach(async t => {
     await t.maximizeWindow()
     page.browserscroll();
     const spinner  = Selector('a').withExactText('Spinner');
     await t.click(spinner);
 })
-
-const lbl      = Selector('label[for=spinner]');
-const input    = Selector('#spinner');
-const visib    = Selector('#disable');
-const toggle   = Selector('#destroy');
-const getval   = Selector('#getvalue');
-const setval   = Selector('#setvalue');
-const up = Selector('body > p:nth-child(1) > span > a:nth-child(1)');
-const down = Selector('body > p:nth-child(1) > span > a:nth-child(2)');
-
 test('button visiblity check', async t => {
     page.browserscroll();
-    await t.switchToIframe('.demo-frame')
+    await t.switchToIframe('.demo-frame');
+    const input    = Selector('#spinner');
+    const visib    = Selector('#disable');
+    const toggle   = Selector('#destroy');
+    const getval   = Selector('#getvalue');
+    const setval   = Selector('#setvalue')
     await t.expect(await input.getAttribute('class')).notContains('disabled');
     await t.expect(await visib.getAttribute('class')).notContains('disabled');
     await t.expect(await toggle.getAttribute('class')).notContains('disabled');
@@ -31,24 +25,34 @@ test('button visiblity check', async t => {
 })
 test('label and input name test', async t => {
     page.browserscroll();
-    await t.switchToIframe('.demo-frame')
-    await t.expect(await lbl.innerText).eql('Select a value:');
+    await t.switchToIframe('.demo-frame');
+    const label      = Selector('label[for=spinner]');
+    const visib    = Selector('#disable');
+    const toggle   = Selector('#destroy');
+    const getval   = Selector('#getvalue');
+    const setval   = Selector('#setvalue')
+    await t.expect(await label.innerText).eql('Select a value:');
     await t.expect(await visib.innerText).eql('Toggle disable/enable');
     await t.expect(await toggle.innerText).eql('Toggle widget');
     await t.expect(await getval.innerText).eql('Get value');
     await t.expect(await setval.innerText).eql('Set value to 5');
 });
-// test('Input buttons check', async t => {
-//     page.browserscroll();
-//     await t.switchToIframe('.demo-frame')
-//     await t.click(up);
-//     await t.expect(await setval.innerText).eql(1);
-//     await t.click(down);
-//     await t.expect(await setval.innerText).eql(0);
-// })
+test('Input buttons check', async t => {
+    page.browserscroll();
+    const up = Selector('body > p:nth-child(1) > span > a.ui-button.ui-widget.ui-spinner-button.ui-spinner-up.ui-corner-tr.ui-button-icon-only');
+    const input    = Selector('#spinner');
+    const down = Selector('body > p:nth-child(1) > span > a.ui-button.ui-widget.ui-spinner-button.ui-spinner-down.ui-corner-br.ui-button-icon-only');
+    await t.switchToIframe('.demo-frame');
+    await t.click(up);
+    await t.expect(await input.value).eql('1');
+    await t.click(down);
+    await t.expect(await input.value).eql('0');
+})
 test('Input Enabled and disable button check', async t => {
         page.browserscroll();
-        await t.switchToIframe('.demo-frame')
+        await t.switchToIframe('.demo-frame');
+        const input    = Selector('#spinner');
+        const visib    = Selector('#disable');
         await t.click(visib);
         await t.expect(await input.hasAttribute('disabled')).ok();
         await t.click(visib);
@@ -56,7 +60,9 @@ test('Input Enabled and disable button check', async t => {
 })
     test('Toggle widget check', async t => {
         page.browserscroll();
-        await t.switchToIframe('.demo-frame')
+        await t.switchToIframe('.demo-frame');
+        const input    = Selector('#spinner');
+        const toggle   = Selector('#destroy');        const getval   = Selector('#getvalue');
         await t.click(toggle);
         const heightval2 = await input.getStyleProperty('height');
         const widthval2 = await input.getStyleProperty('width');
@@ -71,18 +77,41 @@ test('Input Enabled and disable button check', async t => {
 
 test('Togle Enabled and disable button check', async t => {
     page.browserscroll();
-    await t.switchToIframe('.demo-frame')
+    await t.switchToIframe('.demo-frame');
+    const input    = Selector('#spinner');
+    const toggle   = Selector('#destroy');
     await t.click(toggle);
     await t.expect(await input.hasAttribute('disabled')).notOk();
 })
-    test('Get value', async t =>{
+    test.skip('Get value alert check', async t =>{
         page.browserscroll();
         await t.switchToIframe('.demo-frame')
+        .setNativeDialogHandler((type, text, url) => {
+            switch (type) {
+                case 'confirm':
+                    switch (text) {
+                        case 'Do you want to subscribe?':
+                            return false;
+                        case 'Do you want to delete your account?':
+                            return true;
+                        default:
+                            throw 'Unexpected confirm dialog!';
+                    }
+                case 'prompt':
+                    return 'Hi there';
+                case 'alert':
+                    t
+                    .pressKey('enter')
+                    throw 'An alert was invoked!';
+            }
+        })
         .click(getval)
-        .setNativeDialogHandler(() => true)
+        .pressKey('enter')
     })
     test('Set value', async t =>{
         page.browserscroll();
+        const input    = Selector('#spinner');
+        const setval   = Selector('#setvalue');
         await t.switchToIframe('.demo-frame')
         .click(setval);
         await t.expect(input.value).eql('5');
